48c3e18858c12c85064c31e371d48850f22105bc
vrathsmill <vlrathsmill@gmail.com>
2017-02-08 17:08:06 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+require 'pry'
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def make_word_letters
+  @word.split("").sort
+end
+
+
+def match(anagrams)
+  result = []
+  anagrams.collect do |word|
+    letters_array = word.split("")
+    if make_word_letters == letters_array.sort
+      result.push(word)
+    end
+  end
+  result 
+end
+
+end
